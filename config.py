ce6d3f68e7822ec5f04800b2f39e3ea0d1e2290b
aliiiu <ayokunumi99@gmail.com>
2022-06-07 19:26:42 +0100

    first submission

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,4 +11,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/fyyurr'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
